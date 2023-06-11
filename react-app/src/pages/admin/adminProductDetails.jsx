8adeacf43ea38d88bdd7949c4c2ff030bedb5bbc
Bruno Berndt Lima <brunolima674@gmail.com>
2023-06-11 12:27:15 -0300

    Função do admin remover produto  adicionada

--- react-app/src/pages/admin/adminProductDetails.jsx
+++ react-app/src/pages/admin/adminProductDetails.jsx
@@ -1,33 +1,36 @@
-import { useNavigate, useParams } from "react-router-dom";
-import useFetch from "../../components/useFetch";
+import React from 'react';
 
-const ProductDetails = () => {
-    const { id } = useParams();
-    const {data: product, error, isPending} = useFetch('http://localhost:7000/products/' + id);
-    const navigate = useNavigate();
-    
-    const handleClick = () => {
-        fetch('http://localhost:7000/products/' + product.id, {
-            method: 'DELETE'
-        }).then(() =>  {
-            navigate('/');
-        })
-    }
-
+const ProductDetails = ({result}) => {
     return ( 
-        <div className="product-details">
-            { isPending && <div>Loading...</div> }
-            { error && <div>{ error }</div> }
-            { product && (
-                <article>
-                    <h2>{ product.model }</h2>
-                    <p>Written by { product.brand }</p>
-                    <div> { product.body }</div>
-                    <button onClick={handleClick}>Delete</button>
-                </article>
-            )}
+        <div className="product-preview" key={result.id}>
 
-        </div>
+            <div className="product-img">
+                <img src={result.image} alt="" />
+            </div>
+
+            <div className="product-model">
+                <h2>{result.model}</h2>
+            </div>
+
+            <div className="product-brand">
+                <p>{result.brand}</p>
+            </div>
+
+            <div className="product-price">
+                <p>R$ {result.price}</p>
+            </div>
+
+            <div className="product-sizes">
+            <h3>Sizes:</h3>
+            <ul>
+            {result.sizes.map((sizeObj, index) => (
+                <li key={index}>
+                Size: {sizeObj.size}, Quantity: {sizeObj.stock}
+                </li>
+                ))}
+                </ul>
+            </div>
+    </div>
      );
 }
  
