a55355c9bbc123f97850ab223ad63e732f2f78ff
Bruno Berndt Lima <brunolima674@gmail.com>
2023-07-09 19:31:58 -0300

    added change user type

---
+++ react-app/src/components/ChangeUserType.jsx
@@ -0,0 +1,41 @@
+import './ChangeUserType.css'
+
+const ChangeUserType = ({ user, isAdmin, onUserTypeChange }) => {
+  const updateUserType = async () => {
+    try {
+      const updatedUser = {
+        ...user,
+        admin: !isAdmin,
+      };
+
+      const response = await fetch(`http://localhost:7000/users/${user._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedUser),
+      });
+
+      if (response.ok) {
+        onUserTypeChange(!isAdmin);
+        console.log('User type updated successfully');
+      } else {
+        console.log('User type update failed');
+      }
+    } catch (error) {
+      console.error('Error updating user type:', error);
+    }
+  };
+
+  return (
+    <>
+      {isAdmin ? (
+        <button onClick={updateUserType}>Change to Normal User</button>
+      ) : (
+        <button onClick={updateUserType}>Change to Admin</button>
+      )}
+    </>
+  );
+};
+
+export default ChangeUserType;
